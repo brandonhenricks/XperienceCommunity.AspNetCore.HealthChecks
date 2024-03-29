6602b7fc8ee68de348068fc2d7078557fc524daa
Brandon Henricks <78803523+bluemodus-brandon@users.noreply.github.com>
2024-03-29 16:09:39 -0400

    The most significant changes involve the addition of a new health check for Staging Tasks and the creation of a new file `StagingTaskHealthCheck.cs` that contains the implementation of this health check. The `StagingTaskHealthCheck` class has several dependencies that are injected via its constructor and it includes methods for performing the health check and creating a dictionary from a list of `SynchronizationInfo` objects.
    1. A new health check named "Staging Task Health Check" has been added to the `DependencyInjection.cs` file in the `XperienceCommunity.AspNetCore.HealthChecks` namespace. This health check is designed to monitor the status of Staging Tasks.
    
    2. A new file `StagingTaskHealthCheck.cs` has been created. This file contains the `StagingTaskHealthCheck` class, which implements the `IHealthCheck` interface.
    
    3. The `StagingTaskHealthCheck` class has several dependencies: `IStagingTaskInfoProvider`, `IProgressiveCache`, `ISiteService`, and `ISynchronizationInfoProvider`. These dependencies are injected via the class constructor.
    
    4. The `CheckHealthAsync` method in the `StagingTaskHealthCheck` class performs the actual health check. It fetches staging tasks and synchronization tasks from the cache, checks if there are any tasks, and if there are, it checks if there are any synchronization tasks with errors. Depending on the results, it returns a `HealthCheckResult` indicating the health status.
    
    5. The `GetData` method in the `StagingTaskHealthCheck` class is a helper method that creates a dictionary from a list of `SynchronizationInfo` objects. This dictionary is used to provide additional data in the `HealthCheckResult`.
    
    6. Several namespaces have been imported at the beginning of the `StagingTaskHealthCheck.cs` file to provide necessary classes and interfaces for the implementation.

---
+++ src/XperienceCommunity.AspNetCore.HealthChecks/HealthChecks/StagingTaskHealthCheck.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.ObjectModel;
+using System.Data;
+using CMS.Base;
+using CMS.Helpers;
+using CMS.Synchronization;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace XperienceCommunity.AspNetCore.HealthChecks.HealthChecks
+{
+    public sealed class StagingTaskHealthCheck : IHealthCheck
+    {
+        private readonly IStagingTaskInfoProvider _stagingTaskInfoProvider;
+        private readonly IProgressiveCache _cache;
+        private readonly ISiteService _siteService;
+        private readonly ISynchronizationInfoProvider _synchronizationInfoProvider;
+
+        public StagingTaskHealthCheck(IStagingTaskInfoProvider stagingTaskInfoProvider, IProgressiveCache progressiveCache, ISiteService siteService, ISynchronizationInfoProvider synchronizationInfoProvider)
+        {
+            _stagingTaskInfoProvider =
+                stagingTaskInfoProvider ?? throw new ArgumentNullException(nameof(stagingTaskInfoProvider));
+            _cache = progressiveCache;
+            _siteService = siteService;
+            _synchronizationInfoProvider = synchronizationInfoProvider;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            try
+            {
+                var data = await _cache.LoadAsync(async cs =>
+                    {
+                        var results = await _stagingTaskInfoProvider.Get()
+                            .GetEnumerableTypedResultAsync(CommandBehavior.CloseConnection, true, cancellationToken)
+                            .ConfigureAwait(false);
+
+                        cs.CacheDependency = CacheHelper.GetCacheDependency($"{StagingTaskInfo.OBJECT_TYPE}|all");
+
+                        return results;
+                    }, new CacheSettings(TimeSpan.FromMinutes(10).TotalMinutes, $"apphealth|{StagingTaskInfo.OBJECT_TYPE}"))
+                    .ConfigureAwait(false);
+
+                var stagingTasks = data.ToList();
+
+                if (stagingTasks.Count == 0)
+                {
+                    return HealthCheckResult.Healthy("No Staging Tasks Found.");
+                }
+
+                var stagingTaskIdList = stagingTasks
+                    .Where(s => s.TaskSiteID == _siteService.CurrentSite.SiteID)
+                    .Select(x => x.TaskID)
+                    .ToList();
+
+                var syncData = await _cache.LoadAsync(async cs =>
+                    {
+                        var results = await _synchronizationInfoProvider.Get()
+                            .GetEnumerableTypedResultAsync(CommandBehavior.CloseConnection, true, cancellationToken)
+                            .ConfigureAwait(false);
+
+                        cs.CacheDependency = CacheHelper.GetCacheDependency($"{SynchronizationInfo.OBJECT_TYPE}|all");
+
+                        return results;
+                    }, new CacheSettings(TimeSpan.FromMinutes(10).TotalMinutes, $"apphealth|{SynchronizationInfo.OBJECT_TYPE}"))
+                    .ConfigureAwait(false);
+
+                var syncTasks = syncData.ToList();
+
+                if (syncTasks.Count == 0)
+                {
+                    return HealthCheckResult.Healthy("No Synchronization Tasks Found.");
+                }
+
+                var syncErrorTasks = syncTasks
+                     .Where(s => stagingTaskIdList.Contains(s.SynchronizationTaskID) && !string.IsNullOrWhiteSpace(s.SynchronizationErrorMessage))
+                    .ToList();
+
+                if (syncErrorTasks.Count == 0)
+                {
+                    return HealthCheckResult.Healthy("No Synchronization Tasks Contain Errors.");
+                }
+
+                return HealthCheckResult.Degraded("Failed Staging Tasks Found", null, GetData(syncErrorTasks));
+            }
+            catch (InvalidOperationException ex)
+            {
+                return HealthCheckResult.Degraded(ex.Message, ex);
+            }
+            catch (Exception e)
+            {
+                return HealthCheckResult.Unhealthy(e.Message, e);
+            }
+        }
+
+        private static IReadOnlyDictionary<string, object> GetData(IEnumerable<SynchronizationInfo> objects)
+        {
+            var dictionary = objects.ToDictionary<SynchronizationInfo, string, object>(searchTask => searchTask.SynchronizationTaskID.ToString(), searchTask => searchTask.SynchronizationErrorMessage);
+
+            return new ReadOnlyDictionary<string, object>(dictionary);
+        }
+    }
+}
